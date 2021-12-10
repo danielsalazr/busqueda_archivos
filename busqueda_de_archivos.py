ebbf560e9a77621df90e90a9d51731e001d4bab8
colaborador1 <Colaboradorg1@gmail.com>
2021-12-09 21:15:42 -0500

    anadiendo funcion de busqueda

--- busqueda_de_archivos.py
+++ busqueda_de_archivos.py
@@ -4,9 +4,9 @@ import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog
 from PyQt5.QtGui import QIcon
 from PyQt5 import QtCore
-from numpy import add
+from numpy import add, promote_types
 
-ask =  False
+#res =  False
 
 class App(QWidget):
 
@@ -17,20 +17,44 @@ class App(QWidget):
         self.top = 10
         self.width = 640
         self.height = 480
+        self.res= False
+        self.ask=''
         self.initUI()
 
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
-        self.cargar() 
+        try:
+            self.path = self.cargar() 
+        except:
+            print('No tienes direccion de uso reciente')
+        print(f'Usted recientemente busco en la direccion \n {self.path}')
+
+        t=False
+        while t == False:
+            #print(type(self.ask))
+            print((self.ask))
+            self.ask = input('Desea buscar en la misma direccion o desea cambiar carpeta   y/n ?\n')
+
+            if self.ask == 'y':
+                self.res = True
+                t = True
+            else:
+                t = True
+
 
         #self.openFileNameDialog()
         #self.openFileNamesDialog()
         #self.saveFileDialog()
-        addrr = self.showAddress()
+        if  self.ask == 'n':
+            addrr = self.showAddress()
+            if addrr:
+                self.salvar(addrr)
+            #self.show()
 
-        self.salvar(addrr)
-        #self.show()
+    def busquedaArchivo(self, condicion):
+        if condicion == True:
+            pass
 
     def salvar(self, addr):
             shelfFile = shelve.open('dir')
@@ -40,8 +64,10 @@ class App(QWidget):
 
     def cargar(self):
         shelfFile = shelve.open('dir')
-        print(shelfFile['path'])
+        var = str(shelfFile['path'])
         shelfFile.close()
+        #print(var)
+        return var
 
     def showAddress(self, directory=''): # C:/
         #options = QFileDialog.Options()
